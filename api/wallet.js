6192578c67a488d993ddd3e5a328d360468e794d
Lets-go-free <chris@letsgofree.me>
2026-01-04 21:50:06 +0100

    .

---
+++ api/wallet.js
@@ -0,0 +1,15 @@
+﻿export default async function handler(req, res) {
+  const { wallet, chain } = req.query;
+
+  const response = await fetch(
+    `https://deep-index.moralis.io/api/v2.2/${wallet}/erc20?chain=${chain}`,
+    {
+      headers: {
+        "X-API-Key": process.env.MORALIS_API_KEY
+      }
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
